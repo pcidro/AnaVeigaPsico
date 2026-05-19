66f1035590c7035bcf60a84a01395003346d0224
Paulo Cidro <paulocidro@gmail.com>
2026-05-18 22:18:39 -0300

    ajustando funcao get data com revalidate

--- src/actions/getdata.ts
+++ src/actions/getdata.ts
@@ -2,6 +2,11 @@ export async function getDataHome() {
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B"type":"posts"%7D&limit=10&skip=0&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata,type`,
+      {
+        next: {
+          revalidate: 60,
+        },
+      },
     );
     if (!res.ok) {
       throw new Error("Failed to fetch data");
